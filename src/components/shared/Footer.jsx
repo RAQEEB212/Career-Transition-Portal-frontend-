fb04d9f70f5e2508cb5b455f50b1babd87a3a7dc
RAQEEB212 <saneelmemon45@gmail.com>
2025-05-26 02:03:15 +0500

    remains change colors and icon  have to add

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -6,7 +6,9 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <h2 className="text-xl font-bold">Developed by  Mustafa</h2>
+            <h2 className="font-bold ">Developed by  Mohammad Mustafa</h2>
+            <h2 className="text-sm mt-2 ">mohammad.mustafa@bahcesehir.edu.tr</h2>
+            <h2 className="text-sm mt-2 ">+90 (531) 4686196</h2>
           </div>
           
           <div className="flex space-x-4 mt-4 md:mt-0">
